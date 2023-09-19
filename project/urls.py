9acd0bae077638d607e2f5ecfad6cc318a834c0c
jod431047 <jod431047@gmail.com>
2023-09-19 22:57:11 +0200

    add courses_list & courses_detail in urls

--- project/urls.py
+++ project/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from courses.views import CourseList , CourseDetail
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('course/',CourseList.as_view()),
+    path('course/<int:pk>',CourseDetail.as_view()),
 ]
 
 
